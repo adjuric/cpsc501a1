bbac1c8e4b69f789870f63de41148ff4771b63ab
Aleks <Aleks@Aleks-PC>
2016-10-14 07:22:11 -0600

    Adding Test Cases for Refactored Code

---
+++ src/as1Test.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class as1Test {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+	
+	public void testCalcNumCompare(){
+		fail("Not yet implemented");
+	}
+	
+	public void testReadFile(){
+		fail("Not yet implemented");
+	}
+	
+	public void testFindKey(){
+		fail("Not yet implemented");
+	}
+
+}
